16a902ac828cca9a14beace63f60f955731ef14f
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-10-25 14:11:14 +0530

    minor changes done in experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -48,8 +48,11 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>
-          What I have done so far.</p>
-        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+          What I have done so far.
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Work Experience.
+        </h2>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
